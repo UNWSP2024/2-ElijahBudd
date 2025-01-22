944a3734574f8ff7fe008a53130c3db1dce1ff86
ElijahBudd <eabudd@students.unwsp.edu>
2025-01-22 12:50:33 -0600

    Personal Information.py

--- program_1.py
+++ program_1.py
@@ -1,6 +1,4 @@
-def personal_information():
-    print('...')
-
-
-# Line which calls the above function.
-personal_information()
+print("Elijah Budd")
+print("41 4th St, Eltopia, WA 99330")
+print("612-819-5377")
+print("Software Engineering")
