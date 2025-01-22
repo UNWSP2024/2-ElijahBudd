a62264172ddede1e50e52b6ad625239fdb8b720c
ElijahBudd <eabudd@students.unwsp.edu>
2025-01-22 12:55:35 -0600

    Update and rename program_1.py to Personal Information.py

---
+++ Personal Information.py
@@ -0,0 +1,7 @@
+def personal_information():
+    print('Elijah Budd')
+    print('41 4th St, Eltopia, WA 99330')
+    print('612-819-5377')
+    print('Software Engineering')
+# Line which calls the above function.
+personal_information()
--- program_1.py
+++
@@ -1,4 +0,0 @@
-print("Elijah Budd")
-print("41 4th St, Eltopia, WA 99330")
-print("612-819-5377")
-print("Software Engineering")
