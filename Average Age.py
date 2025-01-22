1910485a0d4bf6360dbfb7d3bb6912e07c31bef0
ElijahBudd <eabudd@students.unwsp.edu>
2025-01-22 12:58:45 -0600

    Update and rename program_2.py to Average Age.py

---
+++ Average Age.py
@@ -0,0 +1,18 @@
+def average_age():
+    
+    # Get User Input
+    age1 = int(input("Enter age of Friend 1: "))
+    age2 = int(input("Enter age of Friend 2: "))
+    age3 = int(input("Enter age of Friend 3: "))
+    age4 = int(input("Enter age of Friend 4: "))
+    age5 = int(input("Enter age of Friend 5: "))
+    
+    # Sum ages
+    sum_age = (age1 + age2 + age3 + age4 + age5)
+    
+    # Average the ages
+    average_age = sum_age /5
+    # Print the results
+    print(average_age)
+# Line which calls the above function.
+average_age()
